66ee432d6bbc0fbe8c15c0e667c186d7a3f393b5
TanDuy274 <phanduy27042004@gmail.com>
2025-10-17 23:20:07 +0700

    add userModel

---
+++ e-learning-backend/models/UserModel.js
@@ -0,0 +1,36 @@
+// models/UserModel.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"], // Bắt buộc phải có tên
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true, // Email không được trùng
+      match: [
+        // Kiểm tra định dạng email hợp lệ
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6, // Mật khẩu tối thiểu 6 ký tự
+    },
+    role: {
+      type: String,
+      enum: ["student", "teacher"], // Vai trò chỉ có thể là student hoặc teacher
+      default: "student",
+    },
+  },
+  {
+    timestamps: true, // Tự động thêm 2 trường createdAt và updatedAt
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
