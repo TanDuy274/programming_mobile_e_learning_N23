514f35cdf83e5264ed86e5e04c6b9746e8f7bfcf
TanDuy274 <phanduy27042004@gmail.com>
2025-10-17 23:33:10 +0700

    add route getMe with protect

--- e-learning-backend/routes/authRoutes.js
+++ e-learning-backend/routes/authRoutes.js
@@ -1,12 +1,19 @@
 // routes/authRoutes.js
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/authController");
+const { protect } = require("../middlewares/authMiddleware");
 
 // Route đăng ký
 router.post("/register", registerUser);
-
 // Route đăng nhập
 router.post("/login", loginUser);
+// Khi có request GET tới /me, nó sẽ chạy qua middleware `protect` trước
+// Nếu token hợp lệ, `protect` sẽ gọi `next()` và hàm `getMe` sẽ được thực thi.
+router.get("/me", protect, getMe);
 
 module.exports = router;
