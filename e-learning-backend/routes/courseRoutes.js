a2be404287ab0b22a72a31347da39190b54fbeaf
TanDuy274 <phanduy27042004@gmail.com>
2025-10-17 23:54:48 +0700

    add review routes into course routes

--- e-learning-backend/routes/courseRoutes.js
+++ e-learning-backend/routes/courseRoutes.js
@@ -3,11 +3,15 @@ const express = require("express");
 const router = express.Router();
 const { getCourses, createCourse } = require("../controllers/courseController");
 const { protect } = require("../middlewares/authMiddleware");
+const reviewRouter = require("./reviewRoutes");
 
 // Dùng cách viết gọn
 router
   .route("/")
   .get(getCourses) // Ai cũng có thể xem khóa học
   .post(protect, createCourse); // Chỉ người đã đăng nhập (và là teacher) mới được tạo
+
+// Chuyển hướng các request tới /api/courses/:courseId/reviews cho reviewRouter xử lý
+router.use("/:courseId/reviews", reviewRouter);
 
 module.exports = router;
