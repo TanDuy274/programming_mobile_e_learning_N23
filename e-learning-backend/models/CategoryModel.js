a01ff772febb00499514a4f9bd5b1034f12a3f34
TanDuy274 <phanduy27042004@gmail.com>
2025-10-17 23:37:10 +0700

    add category

---
+++ e-learning-backend/models/CategoryModel.js
@@ -0,0 +1,13 @@
+// models/CategoryModel.js
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a category name"],
+    unique: true,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("Category", categorySchema);
