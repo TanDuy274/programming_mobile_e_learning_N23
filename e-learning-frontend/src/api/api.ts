4b1c3c1092744cb9877578d39f207f8e0bc0b1bc
TanDuy274 <phanduy27042004@gmail.com>
2025-10-18 10:47:33 +0700

    config api

---
+++ e-learning-frontend/src/api/api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+// LƯU Ý QUAN TRỌNG:
+// Dùng địa chỉ IP của máy tính bạn, KHÔNG DÙNG 'localhost'.
+const BASE_URL = "http://localhost:5001/api"; // << THAY ĐỔI ĐỊA CHỈ IP NÀY
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+// Tự động thêm token vào header cho mỗi yêu cầu
+api.interceptors.request.use(async (config) => {
+  const token = await SecureStore.getItemAsync("userToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
