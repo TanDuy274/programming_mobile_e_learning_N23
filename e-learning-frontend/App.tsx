fde1b35596e4cacb7123b9cefd7ff88115fde56b
TanDuy274 <phanduy27042004@gmail.com>
2025-10-18 10:57:40 +0700

    add auth and register screens

--- e-learning-frontend/App.tsx
+++ e-learning-frontend/App.tsx
@@ -2,11 +2,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import "./global.css";
 import { Text, View } from "react-native";
 import TabNavigator from "./src/navigation/TabNavigator";
+import AuthNavigator from "./src/navigation/AuthNavigator";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AuthNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
