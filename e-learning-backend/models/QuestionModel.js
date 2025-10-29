3bfac938652a26056040b6bf81ec896ad66de523
tVhowww <nguyentrunghau7746@gmail.com>
2025-10-29 23:02:30 +0700

    EL-70 add question model

---
+++ e-learning-backend/models/QuestionModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema(
+  {
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Question", questionSchema);
