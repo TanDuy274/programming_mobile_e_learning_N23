a240d7ca37931275a625d21152bd835beaf3b37e
tVhowww <nguyentrunghau7746@gmail.com>
2025-10-30 15:38:59 +0700

    EL-82 update enrollment routes

--- e-learning-backend/routes/enrollmentRoutes.js
+++ e-learning-backend/routes/enrollmentRoutes.js
@@ -1,15 +1,20 @@
-// routes/enrollmentRoutes.js
 const express = require("express");
 const router = express.Router();
 const {
   enrollInCourse,
   getMyCourses,
+  updateEnrollmentProgress,
+  enrollFromCart,
 } = require("../controllers/enrollmentController");
 const { protect } = require("../middlewares/authMiddleware");
 
-// Route này phải đặt trước route có param (:courseId) để tránh bị nhầm lẫn
+// Route để lấy danh sách các khóa học đã ghi danh
 router.get("/my-courses", protect, getMyCourses);
 
+router.route("/checkout").post(protect, enrollFromCart);
+// Route để ghi danh vào một khóa học
 router.post("/:courseId", protect, enrollInCourse);
 
+// << 2. THÊM ROUTE MỚI ĐỂ CẬP NHẬT TIẾN ĐỘ >>
+router.put("/:enrollmentId/progress", protect, updateEnrollmentProgress);
 module.exports = router;
